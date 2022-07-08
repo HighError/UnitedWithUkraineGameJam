f7b4c01c933d34a65b6781f59d79f8d1cf8d890c
HighError <hierrorua@gmail.com>
2022-07-09 01:47:46 +0300

    Tiles

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MapGenerator : MonoBehaviour
 {
-    public Tile[] tileBase;
+
+    public TileBase[] tiles;
+
     private Tilemap map;
     private System.Random rand = new System.Random();
 
-    private void Start(){
+    private void Start()
+    {
+        int tilesInOneCategory = tiles.Length / 4 ;
         map = GetComponent<Tilemap>();
 
 
-        for (int i = 0; i < 30; i++){
-            for (int j = 0; j < 30; j++){
-                int number = rand.Next() % tileBase.Length;
-                map.SetTile(new Vector3Int(i,j,0), tileBase[number]);
+        for (int i = 0; i < 20; i++){
+            for (int j = 0; j < 20; j++){
+                int number = rand.Next() % tiles.Length;
+                map.SetTile(new Vector3Int(i,j,0), tiles[number]);
+                Debug.Log(map.GetTile(new Vector3Int(i, j, 0)));
             }
         }
     }
